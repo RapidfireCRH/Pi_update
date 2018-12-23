78d3741569ce24ee9e10f95a464822286996483a
RapidfireCRH <crhrapidfire@gmail.com>
2018-12-22 17:42:14 -0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,7 +1,12 @@
-wget https://raw.githubusercontent.com/RapidfireCRH/Pi_update/master/updater.sh -o updater.sh.new
+wget https://raw.githubusercontent.com/RapidfireCRH/Pi_update/master/updater.version -o updater.version
 
 if('$(wc -l updater.sh.new)' -eq '$(wc -l updater.sh)') then
-rm updater.sh.new
+    rm updater.sh.new
+else
+    rm updater.sh
+    mv updater.sh.new updater.sh
+    ./updater.sh
+exit
 fi
 
 wget 
