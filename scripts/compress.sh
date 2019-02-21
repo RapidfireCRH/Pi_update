a166a35f366a1cf766263ebd36c28d5bd9adc5e5
RapidfireCRH <crhrapidfire@gmail.com>
2019-02-21 01:55:04 -0700

    Update compress.sh

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -7,30 +7,31 @@ log=$(date +%Y-%m-%d.log)
 dat=$(date)
 
 #cleanup and logging
-echo "Deleting old files in cache">./log/$log 2>&1
-find /home/listener/working/cache -type f -mtime +7>>./log/$log 2>&1
-find /home/listener/working/cache -type f -mtime +7 | xargs -r0 rm -->>./log/$l$
+echo "Deleting old files in cache"
+find /home/listener/working/cache -type f -mtime +7
+find /home/listener/working/cache -type f -mtime +7 | xargs -r0 rm --
 
 #actual compress
-echo "Starting Compress: $dat">>./log/$log 2>&1
-echo "Moving File $yest">>./log/$log 2>&1
-mv ../$yest ./$correctformat>>./log/$log 2>&1
-echo "Moving Log File">>./log/$log 2>&1
-mv ../log/listener.log ./listener.log>>./log/$log 2>&1
-echo "Creating 7zip file">>./log/$log 2>&1
-7zr a "$correctformat.7z" $correctformat listener.log>>./log/$log 2>&1
+echo "Starting Compress: $dat"
+echo "Moving File $yest"
+mv ../$yest ./$correctformat
+echo "Moving Log File"
+mv ../log/listener.log ./listener.log
+echo "Creating 7zip file"
+7zr a "$correctformat.7z" $correctformat listener.log
 
 #post compress
-echo "Deleting listener file">>./log/$log 2>&1
-rm ./listener.log>>./log/$log 2>&1
-echo "moving json file">>./log/$log 2>&1
-mv $correctformat ./cache/$correctformat>>./log/$log 2>&1
-echo "moving 7zip file to completed folder">>./log/$log 2>&1
-mv "$correctformat.7z" "../../webhost/webroot/$correctformat.7z">>./log/$log 2>$
+echo "Deleting listener file"
+rm ./listener.log
+echo "deleting json file"
+rm $correctformat
+echo "moving 7zip file to completed folder"
+mv "$correctformat.7z" "../../webhost/webroot/$correctformat.7z"
 dat=$(date)
-echo "Compress completed: $dat">>./log/$log 2>&1
-echo "Changing ownership">>./log/$log 2>&1
-chown webhost:webhost "../../webhost/webroot/$corretformat.7z">>./log/$log 2>&1
-chmod 664 "../../webhost/webroot/$correctformat.7z">>./log/$log 2>&1
-chown listener:listener "./log/$log">>./log/$log 2>&1
+echo "Compress completed: $dat"
+echo "Changing ownership"
+chown webhost:webhost "../../webhost/webroot/$correctformat.7z"
+chmod 664 "../../webhost/webroot/$correctformat.7z"
+chown listener:listener "./log/$log"
+
 
